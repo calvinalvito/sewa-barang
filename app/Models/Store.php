a562c65c95b3e4757d2b756fd6127fa347a54c99
Calvin <calvinalvito19@gmail.com>
2024-09-19 11:35:03 +0700

    fix: fix import in store model

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Store extends Model
